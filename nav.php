4a4ec3eecee6d531b6e08dfd37cbe7cdc6ee38e0
gozack1 <gozack1@users.noreply.github.com>
2016-12-01 12:33:25 -0800

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,3 +1,11 @@
+<!-- Jason Sneddon - snedd001
+     Roger Delgado - delga051
+     Kristina Nystrom - nystr001
+     Zachary Go - goo06
+     Michael Bobadilla - bobad05
+     CIS 444 - Term Project
+     nav.php
+-->
 <!-- Container for Menu Bar -->
 		 <!-- Menu Bar -->
 		 <ul class="menu">
