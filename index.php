2e95eaf644cbbad40a01432f60595f0d7f0c7c49
gozack1 <gozack1@users.noreply.github.com>
2016-12-01 12:32:02 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
-<!-- CIS 444 - Term Project
+<!-- Jason Sneddon - snedd001
+     Roger Delgado - delga051
+     Kristina Nystrom - nystr001
+     Zachary Go - goo06
+     Michael Bobadilla - bobad05
+     CIS 444 - Term Project
      index.html
 
 
