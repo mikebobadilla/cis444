6204600c3be84a5ccb68dee2f8888c1f5ed96629
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2016-12-06 13:26:03 -0800

    basic cart

--- checkout.php
+++ checkout.php
@@ -1,30 +1,28 @@
 <?php
   include("includes/session.php");
-  var_dump($_COOKIE);
-  var_dump($_POST);
   if(isset($_POST["btn-checkout"])){
     $id = $_POST["flowerID"];
     if(empty($_COOKIE["items"])){
       $cookie = array($id);
       $cookie = serialize($cookie);
-      setcookie("items", $cookie, 86400 * 10);
-      setcookie("count", 1, 86400 * 10);
+      setcookie("items", $cookie, time() + 864000);
+      setcookie("count", 1, time() + 864000);
     } else {
       $count = $_COOKIE["count"] + 1;
       $cookie = $_COOKIE["items"];
       $cookie = unserialize($cookie);
       array_push($cookie, $id);
       $cookie = serialize($cookie);
-      setcookie("items", $cookie, 86400 * 10);
-      setcookie("count", $count, 86400 * 10);
+      setcookie("items", $cookie, time() + 864000);
+      setcookie("count", $count, time() + 864000);
     }
   }
 
   if(isset($_COOKIE["items"])){
     $cookie = $_COOKIE["items"];
     $cookie = unserialize($cookie);
-    echo "+++++++";
-    $res = mysqli_query($link, "SELECT * FROM FLOWERS WHERE FlowerID IN ". implode(", ", $cookie));
+    $sql = "SELECT * FROM FLOWERS WHERE FlowerID IN (". implode(", ", $cookie) . ")";
+    $res = mysqli_query($link, $sql);
   }
 
 ?>
@@ -43,7 +41,35 @@
     </div>
     <div class="wrapper">
       <div class="content">
-
+        <div class="info list">
+          <table>
+            <tr>
+              <th>Flower Image</th>
+              <th>Flower Name</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Remove</th>
+            </tr>
+            <?php
+              while($row = mysqli_fetch_array($res)){
+                $image = $row["Picture"];
+                $name = $row["FlowerName"];
+                $id = $row["FlowerID"];
+                $price = $row["Price"];
+            ?>
+                  <tr>
+                    <td><img class="checkout-img" src="image_folder/<?php echo $image; ?>" alt="<?php echo $name ?>" /></td>
+                    <td><?php echo $name; ?></td>
+                    <td>1</td>
+                    <td><?php echo $price; ?></td>
+                    <td><a onClick="deleteFlower(event, <?php echo $id; ?>)" href="#" >Delete?</a></td>
+                  </tr>
+                  <form id="deleteFlower<?php echo $id ?>" stlye="display:none" action="admin.php" method="post">
+                    <input type="hidden" name="deleteFlower" value="<?php echo $id ?>">
+                  </form>
+             <?php } ?>
+          </table>
+        </div>
     </div>
   </body>
 </html>
